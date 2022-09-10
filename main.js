36839a6d5bc97e107efbfc46d66c354f0a997c86
pulsar1812 <cluster000@gmail.com>
2022-09-10 22:42:44 +0800

    Finish Part 9

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const primaryHeader = document.querySelector('.primary-header')
 const navToggle = document.querySelector('.mobile-nav-toggle')
 const primaryNav = document.querySelector('.primary-navigation')
 
+const slider = new A11YSlider(document.querySelector('.slider'), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots disabled 480px and up
+    },
+  },
+})
+
 navToggle.addEventListener('click', () => {
   primaryNav.hasAttribute('data-visible')
     ? navToggle.setAttribute('aria-expanded', false)
